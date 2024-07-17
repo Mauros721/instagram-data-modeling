792d51e8d87bab8c2d1b83c7daea3dbe8aaaaa33
Mauros721 <maurosantangeloc@gmail.com>
2024-07-17 10:30:55 +0200

    done

---
+++ src/models.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from sqlalchemy import Column,Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(100), unique=True, nullable=False)
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="author")
+    followers = relationship("Follower", foreign_keys="[Follower.user_to_id]", back_populates="followed")
+    following = relationship("Follower", foreign_keys="[Follower.user_from_id]", back_populates="follower")
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    follower = relationship("User", foreign_keys=[user_from_id], back_populates="following")
+    followed = relationship("User", foreign_keys=[user_to_id], back_populates="followers")
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="posts")
+    media = relationship("Media", back_populates="post")
+    comments = relationship("Comment", back_populates="post")
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum('image', 'video', name='media_type'), nullable=False)
+    url = Column(String(255), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", back_populates="media")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(255), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    author = relationship("User", back_populates="comments")
+    post = relationship("Post", back_populates="comments")
+
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
